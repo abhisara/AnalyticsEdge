d622d54258f45c649572901d3f3829f1ed0c1211
abhisara <abhinav.sarapure@gmail.com>
2015-04-20 13:04:21 -0400

    Streamlined more. Added Text variables.

--- process.R
+++ process.R
@@ -3,8 +3,19 @@
 library(rpart)
 library(rpart.plot)
 library(caTools)
+library(lubridate)
+library(tm)
+library(caret)
+library(e1071)
+library(randomForest)
+library(gbm)
 
 
+#set the working directory and read the files.
+setwd('D:/Userfiles/asarapure/Documents/RStudio/Projects/Analytics_Edge/Kaggle')
+train = read.csv('NYTimesBlogTrain.csv')
+test = read.csv('NYTimesBlogTest.csv')
+sub = read.csv('SampleSubmission.csv')
 
 
 #Preprocessing the data and converting it into a DocumentTermMatrix
@@ -19,20 +30,29 @@ corpusdtm = function(textCol, sparsity , letter)
   
   dtm = DocumentTermMatrix(corpus)
   dtm = removeSparseTerms(dtm, sparsity)
-  colnames(dtm) = paste(letter, colnames(dtm))
+  colnames(dtm) = paste0(letter, colnames(dtm))
   return(as.data.frame(as.matrix(dtm)))
 }
 
+#Creating dtm and adding other variables to it.
+dtma = corpusdtm(train$Abstract , 0.945 , 'a')
+dtmh = corpusdtm(train$Headline, 0.95, 'h')
+dtms = corpusdtm(train$Snippet, 0.945, 's')
 
-corAbs = corpusdtm(train$Abstract , 0.96 , 'a')
-corHeadline = corpusdtm(train$Headline, 0.96, 'h')
-corSnippet = corpusdtm(train$Snippet, 0.96, 's')
+#with 0.945 the names are "acompani" "anew"     
+#"atime"    "aweek"    "awill"    "ayork". Removing last.
+dtma = dtma[,-6]
+dtma = dtma[,-4]
+#dtmh with 0.95 gives hnew for both train and test.
 
-
+#dtms two extra. 
+dtms = dtms[,-6]
+dtms = dtms[,-4]
 
 dtmTrain = cbind(dtma, dtms, dtmh)
 
 dtmTrain$Pop = train$Popular
+#dtmTrain$Pop = as.factor(dtmTrain$Pop) # Converting to factor.
 dtmTrain$NewsDesk = train$NewsDesk
 dtmTrain$secName = train$SectionName
 dtmTrain$subSName = train$SubsectionName
@@ -41,15 +61,100 @@ date = as.Date(train$PubDate)
 dtmTrain$weekday = as.factor(wday(date))
 dtmTrain$isSaturday = as.factor(wday(date) == 6)
 dtmTrain$isSunday  = as.factor(wday(date) == 7)
+dtmTrain$hour = as.factor(hour(train$PubDate))
 
 
 #dtmTrain$isHoliday = 
 #creating a isHoliday variable using conditional expression is difficult. 
 
 
+
+
+#Doing the same for the test data. 
+
+test.dtms = corpusdtm(test$Snippet , 0.945 , 's')
+test.dtmh = corpusdtm(test$Headline , 0.95, 'h')
+test.dtma = corpusdtm(test$Abstract, 0.945, 'a')
+
+#test.dtms has one extra.
+test.dtms = test.dtms[,-5]
+test.dtma = test.dtma[,-5]
+
+dtmTest = cbind(test.dtma, test.dtms, test.dtmh)
+
+
+dtmTest$NewsDesk = test$NewsDesk
+dtmTest$secName = test$SectionName
+dtmTest$subSName = test$SubsectionName
+dtmTest$wrdCount = test$WordCount
+datetest = as.Date(test$PubDate)
+dtmTest$weekday = as.factor(wday(datetest))
+dtmTest$isSaturday = as.factor(wday(datetest) == 6)
+dtmTest$isSunday = as.factor(wday(datetest) == 7)
+dtmTest$hour = as.factor(hour(test$PubDate))
+
+
+
+
+
+
+#splitting the training test for crossvalidation
 spl = sample.split(dtmTrain$Pop , SplitRatio =0.7)
-dtrn = subset(dtmTrain , spl == TRUE)
-dtst = subset(dtmTrain , spl == FALSE)
+dtm.cv.train = subset(dtmTrain , spl == TRUE)
+dtm.cv.test = subset(dtmTrain , spl == FALSE)
+
+
+#CrossValidation
+numFolds = trainControl( method = "cv", number = 20 )
+cpGrid = expand.grid( .cp = seq(0.0001,0.01,0.0005)) 
+
+train(Pop ~ NewsDesk + secName + subSName + wrdCount + weekday + isSaturday + isSunday, data = dtmTrain , method = 'rpart' , trControl = numFolds , tuneGrid = cpGrid)
+
+
+
+#Random Forest. Tune RF
+dtm.rf.train = dtmTrain[,seq(73,80)]
+tuneRF(dtm.rf.train[,-1], dtm.rf.train[,1] , ntreeTry = 1000, stepFactor = 2  ,
+       trace = TRUE , plot = TRUE , doBest = FALSE)
+#get mtry 2 as the best.
+
+
+#Let's test for number of trees and nodesize. 
+
+for (j in 0:2){
+for( i in 0:3){
+  numTrees = 100 + ( 10 * i)
+  ndSize = 1 + (1 * j)
+  spl = sample.split(dtmTrain$Pop , SplitRatio = 0.7)
+  train.data = subset(dtmTrain, spl == TRUE)
+  test.data = subset(dtmTrain , spl == FALSE)
+
+rftrees = randomForest(Pop ~ NewsDesk + secName + subSName + wrdCount + weekday + isSaturday + isSunday + hour, mtry = 2,
+             ntree = numTrees , data = train.data , nodesize = ndSize)
+predTrees = predict(rftrees , newdata = test.data)
+RMSES_sum = sum(sqrt((predTrees - test.data$Pop)^2))
+print(paste('trees are ' , numTrees , 'nodesize is ' , ndSize , 'RMSE is ', RMSES_sum))
+}
+}
+
+#Number of trees being too doesn't really help that much. Plus, the results are random each. 
+#
+
+
+
+#correcting the levels of both train and test.
+c = rbind(dtmTrain[,seq(74,81)] , dtmTest[,seq(22, 29)])
+l.test = c[6533:8402,] #Matching the levels
+#train and predict on the test set. 
+rfMod = randomForest(Pop ~ NewsDesk + secName + subSName + wrdCount + weekday + isSaturday + isSunday + hour, mtry = 2, ntree = 119, nodesize = 3, data = dtmTrain)
+predRF = predict(rfMod , newdata = l.test)
+
+
+
+
+
+
+
 
 
 
@@ -61,25 +166,6 @@ summary(pred)
 svMd = svm(Pop ~ . , data = dtrn)
 predSvm = predict(svMd, newdata = dtst)
 summary(predSvm)
-
-
-
-corTestSnip = corpusdtm(test$Snippet , 0.96 , 's')
-corTestHead = corpusdtm(test$Headline , 0.95, 'h')
-corTestAbs = corpusdtm(test$Abstract, 0.95, 'a')
-
-
-dtmTest = cbind(Tdtma, Tdtms, Tdtmh)
-
-
-dtmTest$NewsDesk = test$NewsDesk
-dtmTest$secName = test$SectionName
-dtmTest$subSName = test$SubsectionName
-dtmTest$wrdCount = test$WordCount
-datetest = as.Date(test$PubDate)
-dtmTest$weekday = as.factor(wday(datetest))
-dtmTest$isSaturday = as.factor(wday(datetest) == 6)
-dtmTest$isSunday = as.factor(wday(datetest) == 7)
 
 
 
@@ -117,10 +203,6 @@ glmM = glm(Pop ~ . , data = newTrain )
 predG = predict(glmM , newdata = newTest)
 
 
-
-library(randomForest)
-forest = randomForest(Pop ~ . , data = newTrain[,-1] , ntree = 500 , nodesize = 5)
-predForest = predict(forest, newdata = newTest[,-1])
 
 
 
--- randomForest.R
+++ randomForest.R
@@ -4,23 +4,24 @@
 
 
 #Random Forest. Tune RF
-dtm.rf.train = dtmTrain[,seq(73,80)]
-tuneRF(dtm.rf.train[,-1], dtm.rf.train[,1] , ntreeTry = 1000, stepFactor = 2  ,
+#dtm.rf.train = dtmTrain[,seq(73,80)]
+tuneRF(dtmTrain[,-10], dtmTrain[,10] , ntreeTry = 501, stepFactor = 1.5  ,
        trace = TRUE , plot = TRUE , doBest = FALSE)
 #get mtry 2 as the best.
 
 
 #Let's test for number of trees and nodesize. 
+dtmTrain$Pop = as.integer(dtmTrain$Pop) #Doesn't work for factor Pop.
 
-for (j in 0:2){
+for (j in 0:1){
   for( i in 0:3){
-    numTrees = 100 + ( 10 * i)
-    ndSize = 1 + (1 * j)
+    numTrees = 100 + ( 100 * i)
+    ndSize = 5 + (3 * j)
     spl = sample.split(dtmTrain$Pop , SplitRatio = 0.7)
     train.data = subset(dtmTrain, spl == TRUE)
     test.data = subset(dtmTrain , spl == FALSE)
     
-    rftrees = randomForest(Pop ~ NewsDesk + secName + subSName + wrdCount + weekday + isSaturday + isSunday + hour, mtry = 2,
+    rftrees = randomForest(Pop ~ ., mtry = 6,
                            ntree = numTrees , data = train.data , nodesize = ndSize)
     predTrees = predict(rftrees , newdata = test.data)
     RMSES_sum = sum(sqrt((predTrees - test.data$Pop)^2))
@@ -34,9 +35,21 @@ for (j in 0:2){
 
 
 #correcting the levels of both train and test.
-c = rbind(dtmTrain[,seq(74,81)] , dtmTest[,seq(22, 29)])
+c = rbind(dtmTrain[,seq(11,18)] , dtmTest[,seq(10, 17)])
 l.test = c[6533:8402,] #Matching the levels
+l.t = cbind(dtmTest[,seq(1,9)], l.test)
 #train and predict on the test set. 
-rfMod = randomForest(Pop ~ NewsDesk + secName + subSName + wrdCount + weekday + isSaturday + isSunday + hour, mtry = 2, ntree = 119, nodesize = 3, data = dtmTrain)
-predRF = predict(rfMod , newdata = l.test)
+rfMod = randomForest(Pop ~ ., mtry = 9, ntree = 15001, nodesize = 17, data = dtmTrain)
+#predRF = predict(rfMod , newdata = l.t , type = 'prob')
+predRF = predict(rfMod , newdata = l.t )
+predRF = specify_decimal(predRF, 7)
 
+#predRF = signif( predRF , digits = 6)
+#options(digits = 6) # to not convert it into scientific notation.
+sub$Probability1 = predRF
+View(sub)
+
+write.csv(sub, 'sub3.csv', row.names = FALSE, quote = FALSE)
+
+
+specify_decimal <- function(x, k) format(round(x, k), nsmall=k)
